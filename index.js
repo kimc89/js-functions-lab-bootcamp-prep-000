9d22b488a642d9886a6daa08f82d4551231f5c61
Kimberly Cushing <kimcushing89@gmail.com>
2018-01-07 16:35:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  var greeting = "Happy holidays, ${name}!";
+  var greeting = `Happy holidays, ${name}!`;
   return greeting;
 }
 
 function happyHolidayTo(holiday, name) {
-  var greeting = "Happy ${holiday}, ${name}!";
+  var greeting = `Happy ${holiday}, ${name}!`;
   return greeting;
 }
 
 function holidayCountdown(days, holiday) {
-  var countdown = "It's ${days} until ${holiday}!";
+  var countdown = `It's ${days} until ${holiday}!`;
   return countdown;
 }
 
