92b23e953d5f581c4e96b09b1a15e0cd2751d0e5
Kimberly Cushing <kimcushing89@gmail.com>
2018-01-07 16:18:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // write your code below!
+function happyHolidays(greeting) {
+  greeting = "Happy Holidays!";
+  return happyHolidays;
+}
+
+
+function happyHolidaysTo(greetingwithname) {
+  var name = Josiah;
+  greetingwithname = "Happy Holidays, ${name}!";
+  return happyHolidaysTo;
+}
+
+function happyHolidayTo(greetingnameandholiday) {
+  var holiday = Thanksgiving;
+  greetingnameandholiday = "Happy $[holiday}, ${name}!";
+  return happyHolidaysTo;
+}
+function holidayCountdown(countdown) {
+  var days =13;
+  countdown = "It's ${days} until ${holiday}!";
+  return holidayCountdown;
+}
+
+
+
